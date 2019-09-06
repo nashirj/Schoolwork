a766a741edfd5c7f056bb62726bd9c2d409735b0
Nashir Janmohamed <nashirbass@gmail.com>
2019-09-05 18:34:41 -0700

    first commit of work from SMC CS 20B

---
+++ Class_Activities/Activity1/CountNumbers/src/countnumbers/CountNumbersDriver.java
@@ -0,0 +1,9 @@
+package countnumbers;
+import countnumbers.CountNumbers;
+
+public class CountNumbersDriver {
+	public static void main(String[] args) {
+		int arr[] = {0,1,2,3,4,5,6};
+		System.out.println(CountNumbers.countNumbers(arr));
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Class projects and HW from Irma Ravkic's Data Structures in Java
